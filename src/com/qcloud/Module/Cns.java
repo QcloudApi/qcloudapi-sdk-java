25187750eba233ebd735fa0bba257a605fbc90f9
QcloudApi <QcloudApi@tencent.com>
2015-07-14 20:16:22 +0800

    添加云解析和文智

---
+++ src/com/qcloud/Module/Cns.java
@@ -0,0 +1,7 @@
+package com.qcloud.Module;
+
+public class Cns extends Base {
+	public Cns(){
+		serverHost = "cns.api.qcloud.com";
+	}
+}
---
+++ src/com/qcloud/Module/Wenzhi.java
@@ -0,0 +1,7 @@
+package com.qcloud.Module;
+
+public class Wenzhi extends Base {
+	public Wenzhi(){
+		serverHost = "wenzhi.api.qcloud.com";
+	}
+}
