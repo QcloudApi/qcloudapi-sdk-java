c3cc142d76d3c0b629ef888dd969b041d0431a9d
QcloudApi <QcloudApi@users.noreply.github.com>
2015-11-23 10:14:42 +0800

    简化 Demo.java
    简化示例代码，添加详细使用说明

--- src/Demo.java
+++ src/Demo.java
@@ -1,54 +1,48 @@
 import java.util.TreeMap;
 
 import com.qcloud.QcloudApiModuleCenter;
-import com.qcloud.Module.*;
-import com.qcloud.Utilities.MD5;
-
+import com.qcloud.Module.Cvm;
+import com.qcloud.Utilities.Json.JSONObject;
 
 public class Demo {
 	public static void main(String[] args) {
+		/* 如果是循环调用下面举例的接口，需要从此处开始你的循环语句。切记！ */
 		TreeMap<String, Object> config = new TreeMap<String, Object>();
-//		config.put("SecretId", "你的secretId");
-//		config.put("SecretKey", "你的secretKey");
-//		config.put("RequestMethod", "GET");
-//		config.put("DefaultRegion", "gz");
-//		QcloudApiModuleCenter module = new QcloudApiModuleCenter(new Cvm(), config);
-//		TreeMap<String, Object> params = new TreeMap<String, Object>();
-//		params.put("offset", 0);
-//		params.put("limit", 3);
-//		System.out.println(module.generateUrl("DescribeInstances", params));
-//		System.out.println(module.call("DescribeInstances", params));
-		
 		config.put("SecretId", "你的secretId");
 		config.put("SecretKey", "你的secretKey");
-		config.put("RequestMethod", "POST");
+		/* 请求方法类型 POST、GET */
+		config.put("RequestMethod", "GET");
+		/* 区域参数，可选: gz:广州; sh:上海; hk:香港; ca:北美;等。 */
 		config.put("DefaultRegion", "gz");
-		QcloudApiModuleCenter module = new QcloudApiModuleCenter(new Cdn(), config);
-		TreeMap<String, Object> params = new TreeMap<String, Object>();
-		try{
-//DeleteCdnEntity
-//			params.put("entityFileName", "/upload/del.bat");
-//			System.out.println(module.call("DeleteCdnEntity", params));
 
-//UploadCdnEntity
-			String fileName = "c:\\9EF077317173460C8C7F48CA4F177D2B.png";		
-			params.put("entityFileName", "/upload/9EF077317173460C8C7F48CA4F177D2B.png");
-			params.put("entityFileMd5", MD5.fileNameToMD5(fileName));
-			params.put("entityFile", fileName);
-			System.out.println(module.call("UploadCdnEntity", params));
+		/*
+		 * 你将要使用接口所在的模块，可以从 官网->云api文档->XXXX接口->接口描述->域名
+		 * 中获取，比如域名：cvm.api.qcloud.com，module就是 new Cvm()。
+		 */
+		/*
+		 * 示例：DescribeInstances
+		 * 的api文档地址：http://www.qcloud.com/wiki/v2/DescribeInstances
+		 */
+		QcloudApiModuleCenter module = new QcloudApiModuleCenter(new Cvm(),
+				config);
+
+		TreeMap<String, Object> params = new TreeMap<String, Object>();
+		/* 将需要输入的参数都放入 params 里面，必选参数是必填的。 */
+		/* DescribeInstances 接口的部分可选参数如下 */
+		params.put("offset", 0);
+		params.put("limit", 3);
 		
-//DescribeCdnEntities
-//			params.put("entityBaseDir", "/upload");
-//			System.out.println(module.call("DescribeCdnEntities", params));
-		
-//RenameCdnEntity
-//			params.put("entityFileName", "/upload/del.bat");
-//			params.put("entityNewFileName", "/upload/delete.bat");
-//			System.out.println(module.call("RenameCdnEntity", params));
-		
+		/* generateUrl 方法生成请求串，但不发送请求。在正式请求中，可以删除下面这行代码。 */
+		// System.out.println(module.generateUrl("DescribeInstances", params));
+		String result = null;
+		try {
+			/* call 方法正式向指定的接口名发送请求，并把请求参数params传入，返回即是接口的请求结果。 */
+			result = module.call("DescribeInstances", params);
+			JSONObject json_result = new JSONObject(result);
+			System.out.println(json_result);
+		} catch (Exception e) {
+			System.out.println("error..." + e.getMessage());
 		}
-		catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+
 	}
 }
