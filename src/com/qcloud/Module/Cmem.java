dbe93498b899a5686921f82071cb46cffb7820f7
QcloudApi <QcloudApi@users.noreply.github.com>
2016-07-06 18:41:25 +0800

    增加Cmem模块

---
+++ src/com/qcloud/Module/Cmem.java
@@ -0,0 +1,8 @@
+package com.qcloud.Module;
+
+public class Cmem extends Base {
+	//protected String serverHost = "cmem.api.qcloud.com";
+	public Cmem(){
+		serverHost = "cmem.api.qcloud.com";
+	}
+}
