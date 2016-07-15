1f7fd71441f3097323f3fe354395b47cc955a4d8
QcloudApi <QcloudApi@users.noreply.github.com>
2016-07-15 21:08:55 +0800

    Create Tdsql.java

---
+++ src/com/qcloud/Module/Tdsql.java
@@ -0,0 +1,8 @@
+package com.qcloud.Module;
+
+public class Tdsql extends Base {
+	//protected String serverHost = "tdsql.api.qcloud.com";
+	public Tdsql(){
+		serverHost = "tdsql.api.qcloud.com";
+	}
+}
