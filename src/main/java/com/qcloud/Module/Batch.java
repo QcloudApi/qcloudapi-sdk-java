6744cf99c07518a44dec19eb3693d107e83e5885
ZhiQiang Fan <zhiqiangfan@tencent.com>
2017-12-09 04:31:43 -0800

    add batch module

---
+++ src/main/java/com/qcloud/Module/Batch.java
@@ -0,0 +1,12 @@
+package com.qcloud.Module;
+
+/*
+ * see document: <a
+ * href="https://cloud.tencent.com/document/product/599">
+ * https://cloud.tencent.com/document/product/599</a>
+ */
+public class Batch extends Base {
+    public Batch(){
+        serverHost = "batch.api.qcloud.com";
+    }
+}
