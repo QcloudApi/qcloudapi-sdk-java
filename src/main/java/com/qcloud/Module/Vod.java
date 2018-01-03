2e2da5f0267c657cfc3350afef7080902a3a11bd
ZhiQiang Fan <zhiqiangfan@tencent.com>
2018-01-03 03:45:35 -0800

    remove vod action MultipartUploadVodFile
    MultipartUploadVodFile has been abandoned by product, if user requests
    this action, it will return ActionNotFound. So let's remove it.

--- src/main/java/com/qcloud/Module/Vod.java
+++ src/main/java/com/qcloud/Module/Vod.java
@@ -1,33 +1,7 @@
 package com.qcloud.Module;
 
-import java.io.File;
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-import java.util.TreeMap;
-
-import com.qcloud.Utilities.SHA1;
-
 public class Vod extends Base {
 	public Vod(){
 		serverHost = "vod.api.qcloud.com";
-	}
-	
-	public String MultipartUploadVodFile(TreeMap<String, Object> params) throws NoSuchAlgorithmException, IOException {
-		serverHost = "vod.qcloud.com";
-		
-		String actionName = "MultipartUploadVodFile";
-
-        String fileName = params.get("file").toString();
-        params.remove("file");
-        File f= new File(fileName);  
-        
-        if (!params.containsKey("fileSize")){
-        	params.put("fileSize", f.length());
-        }
-        if (!params.containsKey("fileSha")){
-        	params.put("fileSha", SHA1.fileNameToSHA(fileName));
-        }
-        
-        return call(actionName, params, fileName);
 	}
 }
